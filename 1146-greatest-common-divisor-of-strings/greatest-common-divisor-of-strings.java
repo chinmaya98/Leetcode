2dc1cf7d3626dd772d39b5b3187c22a2c454d4ef
Chinmaya Gayathri <chinmayagayathri98@gmail.com>
2025-04-07 12:53:14 -0700

    Time: 2 ms (17.20%) | Memory: 42.4 MB (46.27%) - LeetSync

--- 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/greatest-common-divisor-of-strings.java
@@ -1,20 +1,22 @@
 class Solution {
     public boolean valid(String str1, String str2, int k) {
-        int len1 = str1.length(), len2 = str2.length();
-        if (len1 % k > 0 || len2 % k > 0) {
+        int i = str1.length();
+        int j = str1.length();
+        if( i % k > 0 || j % k > 0){
             return false;
         } else {
-            String base = str1.substring(0, k);
-            return str1.replace(base, "").isEmpty() && str2.replace(base, "").isEmpty();
+            String base = str1.substring(0,k);
+            return str1.replace(base,"").isEmpty() && str2.replace(base,"").isEmpty();
         }
+      
     }
-    
-    
-    public String gcdOfStrings(String str1, String str2) {
-        int len1 = str1.length(), len2 = str2.length();
-        for (int i = Math.min(len1, len2); i >= 1; --i) {
-            if (valid(str1, str2, i)) {
-                return str1.substring(0, i);
+
+    public String gcdOfStrings(String str1, String str2){
+        int i = str1.length();
+        int j = str2.length();
+        for(int m = Math.min(1,j); i>=1; --i){
+            if(valid(str1,str2,i)){
+                return str1.substring(0,i);
             }
         }
         return "";
