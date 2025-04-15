2b481f85bc41247ba4d10f39dc24fdc6e792bf5e
Chinmaya Gayathri <chinmayagayathri98@gmail.com>
2025-04-14 21:18:11 -0700

    Time: 370 ms (73.55%) | Memory: 0B (100.00%) - LeetSync

--- 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ 570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,16 +1,6 @@
-SELECT 
-    e.name  
-FROM 
-    Employee e
-JOIN (
-    SELECT 
-        managerId
-    FROM 
-        Employee
-    WHERE 
-        managerId IS NOT NULL
-    GROUP BY 
-        managerId
-    HAVING 
-        COUNT(*) >= 5
-) m ON e.id = m.managerId;
+select name
+from Employee 
+where id in (select managerId
+from Employee
+group by managerId
+having count(*)>=5)
