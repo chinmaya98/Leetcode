3b5ece592229dfdddb3395d8631c897957ecf284
Chinmaya Gayathri <chinmayagayathri98@gmail.com>
2025-04-18 12:11:42 -0700

    Time: 593 ms (68.79%) | Memory: 0B (100.00%) - LeetSync

--- 1877-find-followers-count/find-followers-count.sql
+++ 1877-find-followers-count/find-followers-count.sql
@@ -1,4 +1,1 @@
-SELECT user_id, COUNT(follower_id) as followers_count
-FROM Followers
-GROUP BY user_id
-ORDER BY user_id
+SELECT user_id, COUNT(follower_id) as followers_count FROM Followers GROUP BY user_id ORDER BY user_id
