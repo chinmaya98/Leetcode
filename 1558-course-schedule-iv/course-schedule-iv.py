67ae38930928ddf9ae2f45c11f25cef34ff92882
Chinmaya Gayathri <chinmayagayathri98@gmail.com>
2025-06-02 09:53:25 -0700

    Time: 30 ms (80.78%) | Memory: 20.3 MB (94.46%) - LeetSync

--- 1558-course-schedule-iv/course-schedule-iv.py
+++ 1558-course-schedule-iv/course-schedule-iv.py
@@ -1,26 +1,25 @@
 class Solution:
-    def checkIfPrerequisite(self, numCourses: int, prerequisites: List[List[int]], queries: List[List[int]]) -> List[bool]:
+    def checkIfPrerequisite(self, numCou:int, prereq: List[List[int]], queries:List[List[int]]) -> List[bool]:
+        indegree = [0] * numCou
         graph = defaultdict(list)
-        indegree = [0] * numCourses
-        isprereq = [set() for _ in range(numCourses)]
+        isprereq = [set() for _ in range(numCou)]
 
-        for a, b in prerequisites:
+        for a,b in prereq:
             graph[a].append(b)
-            indegree[b] +=1
+            indegree[b] += 1
         
         queue = deque()
-        for n in range(numCourses):
+        for n in range(numCou):
             if indegree[n] == 0:
                 queue.append(n)
-        
 
         while queue:
             node = queue.popleft()
             for nei in graph[node]:
-                indegree[nei] -=1
                 isprereq[nei].add(node)
                 isprereq[nei].update(isprereq[node])
+                indegree[nei] -=1
                 if indegree[nei] == 0:
                     queue.append(nei)
-        return [u in isprereq[v] for u,v in queries]
         
+        return [u in isprereq[v] for u, v in queries]
