cbd99ef6d070737e3643fb3f2ab39e8df4c040bc
Chinmaya Gayathri <chinmayagayathri98@gmail.com>
2025-06-02 09:43:00 -0700

    Time: 7 ms (47.26%) | Memory: 19.2 MB (60.75%) - LeetSync

--- 207-course-schedule/course-schedule.py
+++ 207-course-schedule/course-schedule.py
@@ -1,26 +1,23 @@
 class Solution:
-    def canFinish(self, numCourses:int, prerequisities:List[List[int]]) -> bool:
+    def canFinish(self, numCou : int, prereq:List[List[int]]) -> bool:
+        indegree = [0] * numCou
         graph = defaultdict(list)
-        indegree = [0] * numCourses
 
-        for a,b in prerequisities:
+        for a,b in prereq:
             graph[b].append(a)
             indegree[a] +=1
-
+        
         queue = deque()
-        for n in range(numCourses):
+        for n in range(numCou):
             if indegree[n] == 0:
                 queue.append(n)
-        
-        count = 0
 
+        count = 0
         while queue:
             node = queue.popleft()
-            count += 1
-            for n in graph[node]:
-                indegree[n] -=1
-                if indegree[n] == 0:
-                    queue.append(n)
-        return count == numCourses
-
-
+            count +=1
+            for nei in graph[node]:
+                indegree[nei] -=1
+                if indegree[nei] == 0:
+                    queue.append(nei)
+        return count == numCou
