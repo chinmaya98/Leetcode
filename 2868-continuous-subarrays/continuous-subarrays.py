88f3b0ac49ae671be10bcd330135618fc0ad985c
Chinmaya Gayathri <chinmayagayathri98@gmail.com>
2025-06-12 11:15:33 -0700

    Time: 1045 ms (12.16%) | Memory: 27.8 MB (98.09%) - LeetSync

--- 2868-continuous-subarrays/continuous-subarrays.py
+++ 2868-continuous-subarrays/continuous-subarrays.py
@@ -2,12 +2,12 @@ class Solution:
     def continuousSubarrays(self, nums: List[int]) -> int:
         sl = SortedList()
         l = 0
-        result = 0
-
+        out = 0
         for r in range(len(nums)):
             sl.add(nums[r])
             while sl[-1] - sl[0] > 2:
                 sl.remove(nums[l])
                 l +=1
-            result += r-l+1
-        return result
+            out += r-l+1
+        return out
+        
